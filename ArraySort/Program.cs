a11145de7599471df4a2328b95db356a498cf216
Sergii Mnemonik <sergeyshovkunenko@gmail.com>
2018-04-02 20:08:49 -0700

    Finaly working sort method of 2DArray

--- ArraySort/Program.cs
+++ ArraySort/Program.cs
@@ -23,27 +23,27 @@ namespace ArraySort
             PrintElements(array1);
             Console.ReadLine();
 
-            int[,] array2D = { { 5, 1, 110, 4, 15, 6, 8 }, 
-                               { 99, 4, 1100, 6, 78, 9, 54 }, 
-                                { 15, 11, 110, 14, 15, 6, 18 }, 
-                                 { 299, 4, 1100, 6, 178, 9, 154 } };
+            int[,] array2D = { { 5, 1, 110, 4, 12},
+                               { 99, 44, 1100, 6, 13},
+                                { 15, 11, 110, 14, 66},
+                                 { 299, 444, 1100, 6, -5 } };
             Console.WriteLine("2D Array:");
-            Print2dArray(array2D);
+            Print2DArray(array2D);
             Console.WriteLine("Sorted 2DArray:");
             SimpleSort2D(array2D);
-            Print2dArray(array2D);
+            Print2DArray(array2D);
         }
 
         static void BubbleSort(int[] array)
         {
-            int iter = 0;
-            for (int i = array.Length - 1; i > 0; i--)
+            var iter = 0;
+            for (var i = array.Length - 1; i > 0; i--)
             {
-                for (int j = 0; j < i; j++)
+                for (var j = 0; j < i; j++)
                 {
                     if (array[j] > array[j + 1])
                     {
-                        int tmp = array[j];
+                        var tmp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = tmp;
                         iter++;
@@ -55,22 +55,22 @@ namespace ArraySort
         }
         static void PrintElements(int[] numbers)
         {
-            for (int i = 0; i < numbers.Length; i++)
+            for (var i = 0; i < numbers.Length; i++)
             {
                 Console.WriteLine("{0} {1}", i, numbers[i]);
 
             }
         }
 
-        static void Print2dArray(int[,] numb)
+        private static void Print2DArray(int[,] numb)
         {
 
-            int rowLength = numb.GetLength(0);
-            int colLength = numb.GetLength(1);
+            var rowLength = numb.GetLength(0);
+            var colLength = numb.GetLength(1);
 
-            for (int i = 0; i < rowLength; i++)
+            for (var i = 0; i < rowLength; i++)
             {
-                for (int j = 0; j < colLength; j++)
+                for (var j = 0; j < colLength; j++)
                 {
                     Console.Write(string.Format("{0} ", numb[i, j]));
                 }
@@ -81,68 +81,62 @@ namespace ArraySort
 
         static void SimpleSort(int[] array)
         {
-            int iter = 0;
-            for (int i = 0; i < array.Length - 1; i++)
+            var iter = 0;
+            for (var i = 0; i < array.Length - 1; i++)
             {
-                int min = array[i];
-                int index = i;
-                for (int j = i + 1; j < array.Length; j++)
+                var min = array[i];
+                var index = i;
+                for (var j = i + 1; j < array.Length; j++)
                 {
-                    if (array[j] < min)
-                    {
-                        min = array[j];
-                        index = j;
-                        iter++;
-                    }
+                    if (array[j] >= min) continue;
+                    min = array[j];
+                    index = j;
+                    iter++;
                 }
-                int tmp = array[i];
+                var tmp = array[i];
                 array[i] = array[index];
                 array[index] = tmp;
             }
             Console.WriteLine("Total amount of iterations is {0}", iter);
         }
 
-        static void SimpleSort2D(int[,] arr)
+        private static void SimpleSort2D(int[,] arr)
         {
-            int iter = 0;
-            int n = arr.GetLength(0);
-            int m = arr.GetLength(1);
-            for (int k = 0; k < n; ++k)
+            var iter = 0;
+            var numberOfRows = arr.GetLength(0);
+            var numberofColumns = arr.GetLength(1);
+
+            for (var k = 0; k < numberOfRows; ++k)
             {
-                for (int l = 0; l < m; ++l)
+                for (var l = 0; l < numberofColumns; ++l)
                 {
-                    for (int i = 0; i < n; ++i)
+                    for (var i = 0; i < numberOfRows; ++i)
                     {
-                        for (int j = 0; j < m ; ++j)
+                        for (var j = 0; j < numberofColumns; ++j)
                         {
-                            if ((i + 1) == n && (j + 1) == m)
+                            if ((i + 1) == numberOfRows && (j + 1) == numberofColumns) continue;
+
+                            if ((j + 1 == numberofColumns) && (arr[i, j] > arr[i + 1, 0]))
                             {
-                                continue;
-                            }
-                            else
-                            if ((j + 1 == m) && (arr[i, j] > arr[i + 1, 0]))
-                            {
-                                int temp = arr[i, j];
+                                var temp = arr[i, j];
                                 arr[i, j] = arr[i + 1, 0];
                                 arr[i + 1, 0] = temp;
                                 iter++;
                             }
-
                             else
-                            if (arr[i, j] > arr[i, j + 1])
                             {
-                                int temp = arr[i, j];
+                                if (j + 1 == numberofColumns) continue;
+                                if (arr[i, j] <= arr[i, j + 1]) continue;
+                                var temp = arr[i, j];
                                 arr[i, j] = arr[i, j + 1];
                                 arr[i, j + 1] = temp;
                                 iter++;
                             }
-
                         }
                     }
                 }
                 Console.WriteLine("Total amount of iterations is {0}", iter);
             }
-
         }
     }
 }
